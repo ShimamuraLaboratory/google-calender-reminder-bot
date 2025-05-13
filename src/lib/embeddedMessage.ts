70df7b8b8724c4746b3cd3c054ba597219ac85fc
Daaaai0809 <dai.tsuruga@80and.co>
2025-05-13 20:27:49 +0900

    chore : 送信メッセージのFormat変更

--- src/lib/embeddedMessage.ts
+++ src/lib/embeddedMessage.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import type { SubCommandType } from "@/constant";
 
 type MessageData = {
@@ -30,19 +31,19 @@ export const embeddedMessage = (
 
 const embeddedMessageImpls: Record<SubCommandType, () => string> = {
   add: () => {
-    return "カレンダーに予定が追加されました！\n {event}";
+    return "## カレンダーに予定が追加されました！\n \n {event}";
   },
   list: () => {
-    return "カレンダーの予定一覧です！\n {events}";
+    return "## カレンダーの予定一覧です！\n {events}";
   },
   show: () => {
-    return "カレンダーの予定詳細です！\n {event}";
+    return "## カレンダーの予定詳細です！\n {event}";
   },
   update: () => {
-    return "カレンダーの予定が更新されました！\n {event}";
+    return "## カレンダーの予定が更新されました！\n {event}";
   },
   delete: () => {
-    return "カレンダーの予定が削除されました！\n {event}";
+    return "## カレンダーの予定が削除されました！\n {event}";
   },
 };
 
@@ -52,11 +53,11 @@ const embeddedOptions = (roleIds?: string[], memberIds?: string[]) => {
   }
 
   let options = "";
-  if (roleIds) {
+  if (roleIds && roleIds.length > 0) {
     options += `ロール : @${roleIds.join(", @")} \n`;
   }
 
-  if (memberIds) {
+  if (memberIds && memberIds.length > 0) {
     options += `メンバー : @${memberIds.join(", @")} \n`;
   }
 
@@ -64,13 +65,30 @@ const embeddedOptions = (roleIds?: string[], memberIds?: string[]) => {
 };
 
 const embeddEvent = (event: MessageData) => {
-  return `イベントID: ${event.id} \n タイトル: ${event.title} \n 期間: ${event.startAt} ~ ${event.endAt} \n 詳細: ${event.description} \n ${embeddedOptions(event.options?.roleIds, event.options?.memberIds)} \n [リンク](${event.url})`;
+  const msg = [] as string[];
+  msg.push(`### タイトル: ${event.title} \n`);
+  msg.push(`イベントID: ${event.id} \n`);
+
+  // NOTE: YYYY年MM月DD日 HH:mm形式に変換
+  const startAt = dayjs(event.startAt).format("YYYY年MM月DD日 HH:mm");
+  const endAt = dayjs(event.endAt).format("YYYY年MM月DD日 HH:mm");
+
+  msg.push(`期間: ${startAt} ~ ${endAt} \n`);
+  if (event.description) {
+    msg.push(`詳細: ${event.description} \n`);
+  }
+  if (event.options) {
+    if (event.options.roleIds && event.options.roleIds.length > 0) {
+      msg.push(`ロール : @${event.options.roleIds.join(", @")} \n`);
+    }
+    if (event.options.memberIds && event.options.memberIds.length > 0) {
+      msg.push(`メンバー : @${event.options.memberIds.join(", @")} \n`);
+    }
+  }
+  msg.push(`[リンク](${event.url})`);
+  return msg.join("");
 };
 
 const embeddedEvents = (events: MessageData[]) => {
-  return events
-    .map((event) => {
-      return `イベントID: ${event.id} \n タイトル: ${event.title} \n 期間: ${event.startAt} ~ ${event.endAt} \n 詳細: ${event.description} \n ${embeddedOptions(event.options?.roleIds, event.options?.memberIds)} \n [リンク](${event.url})`;
-    })
-    .join("\n");
+  return events.map((event) => embeddEvent(event)).join("\n");
 };
