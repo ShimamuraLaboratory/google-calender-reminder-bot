bc94c479997d5a773dd9d22fffa3e62809e12701
Daaaai0809 <dai.tsuruga@80and.co>
2025-05-27 19:49:38 +0900

    feat : cron Job, Handlerの実装 #32

--- src/tokens.ts
+++ src/tokens.ts
@@ -4,16 +4,20 @@ export const TOKENS = {
   SubscribeCommandHandler: Symbol.for("SubscribeCommandHandler"),
   InteractiveHandler: Symbol.for("InteractiveHandler"),
   ModalHandler: Symbol.for("ModalHandler"),
+
   CommandService: Symbol.for("CommandService"),
   FetchServerInfoService: Symbol.for("FetchServerInfoService"),
   InteractionService: Symbol.for("InteractionService"),
   ModalService: Symbol.for("ModalService"),
   SubscribeService: Symbol.for("SubscribeService"),
+  ReminderService: Symbol.for("ReminderService"),
+
   ScheduleRepository: Symbol.for("ScheduleRepository"),
   MemberRepository: Symbol.for("MemberRepository"),
   RoleRepository: Symbol.for("RoleRepository"),
   RemindRepository: Symbol.for("RemindRepository"),
   DiscordClient: Symbol.for("DiscordClient"),
+
   DISCORD_TOKEN: Symbol.for("DISCORD_TOKEN"),
   DISCORD_PUBLIC_KEY: Symbol.for("DISCORD_PUBLIC_KEY"),
   DISCORD_APP_ID: Symbol.for("DISCORD_APP_ID"),
